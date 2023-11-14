a2ea26def45c5ff3496c24a008afe45330557550
Rehan-Shield <rehan.tosif@shield.bh>
2023-11-14 07:32:35 +0300

    adding trasports to socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -5,4 +5,6 @@ const URL =
     ? (process.env.NEXT_PUBLIC_PROD_SOCKET_URL as string)
     : (process.env.NEXT_PUBLIC_DEV_SOCKET_URL as string);
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  transports: ["websocket", "polling"],
+});
